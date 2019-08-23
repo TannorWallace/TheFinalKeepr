98167c48846a6f318f34545057c8dc7c8ff30a19
TannorWallace <tannor.wallace@gmail.com>
2019-08-23 15:04:17 -0600

    First commit BEEEWM!

--- db-setup.sql
+++ db-setup.sql
@@ -1,3 +1,5 @@
+
+
 -- CREATE TABLE users (
 --     id VARCHAR(255) NOT NULL,
 --     username VARCHAR(20) NOT NULL,
@@ -60,7 +62,7 @@
 -- )
 
 
--- -- USE THIS LINE FOR GET KEEPS BY VAULTID
--- SELECT * FROM vaultkeeps vk
--- INNER JOIN keeps k ON k.id = vk.keepId 
--- WHERE (vaultId = @vaultId AND vk.userId = @userId) 
+-- -- -- USE THIS LINE FOR GET KEEPS BY VAULTID
+-- -- SELECT * FROM vaultkeeps vk
+-- -- INNER JOIN keeps k ON k.id = vk.keepId 
+-- -- WHERE (vaultId = @vaultId AND vk.userId = @userId) 
--- wwwsrc/src/main.js
+++ wwwsrc/src/main.js
@@ -5,7 +5,7 @@ import store from './store'
 import AuthService from "./AuthService"
 
 //Vue.config.productionTip = false
-
+//REVIEW DO NOT TOUCHT THIS SECTION OF THE FILE AUTH IS DONE!!!
 async function init() {
   let user = await AuthService.Authenticate()
   if (user) { store.commit("setUser", user) }
